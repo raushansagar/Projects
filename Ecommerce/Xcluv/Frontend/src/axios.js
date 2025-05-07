e015279ccf9ce12f1902607f031dcfcf723554b5
RaushanSagar1694 <dayasagar1694@gmail.com>
2025-05-08 00:29:13 +0530

    d

--- Ecommerce/Xcluv/Frontend/src/axios.js
+++ Ecommerce/Xcluv/Frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: 'https://xcluv-backend.onrender.com',
+    baseURL: 'https://xcluv-backend.onrender.com/xcluv/v2/users/',
     withCredentials: true,
   });
   
