6e4cb018b459c78e6594cc0c66f1de209d50756e
RaushanSagar1694 <dayasagar1694@gmail.com>
2025-05-10 13:02:32 +0530

    add app

--- Ecommerce/Xcluv/Frontend/src/App.jsx
+++ Ecommerce/Xcluv/Frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        {loginPopUp ? <Route path='/Shop' element={<DisplayItems />} /> : <Route path='/' element={<DisplayItems />} />}
+        {loginPopUp ? <Route path='/Shop' element={<DisplayItems />} /> : <Route path='/' element={<Home />} />}
         <Route path='/New' element={<Home />} />
         <Route path='/Offers' element={<NewArrivals />} />
         <Route path='/Contact' element={<Home />} />
