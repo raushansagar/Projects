e31cf598ed1dc8d5db9d11d1df97bde15f5b6375
RaushanSagar1694 <dayasagar1694@gmail.com>
2025-11-20 23:59:34 +0530

    add send otp

--- Ping/Backend/src/utils/otpControler.js
+++ Ping/Backend/src/utils/otpControler.js
@@ -32,7 +32,8 @@ const sendOtp = async (req, res) => {
 
     // Create transporter
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",   
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
